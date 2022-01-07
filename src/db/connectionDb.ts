5dc61fd9f8f9a8f629153124dfa61f2575805b50
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-06 20:05:09 -0600

    HOST

--- src/db/connectionDb.ts
+++ src/db/connectionDb.ts
@@ -7,7 +7,7 @@ export const sequelizeDB = new Sequelize(
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "root",
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "127.0.0.1'",
     port: 3306,
     dialect: "mysql",
   }
