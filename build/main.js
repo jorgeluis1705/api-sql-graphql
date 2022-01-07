a6d4ae99f60d04459831309fff44257056edf993
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-07 14:30:12 -0600

    removed build

--- build/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-const server = new server_1.ServerExpress();
-server.listener();
--- build/resolvers/resolvers.map.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolversMap = void 0;
-const query_1 = require("./query");
-const type_1 = require("./type");
-const mutation_1 = require("./mutation");
-exports.resolversMap = Object.assign(Object.assign(Object.assign({}, query_1.query), type_1.type), mutation_1.mutation);
