508fb9e2f85549c9207cde95c59801acc8f93372
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-06 19:25:06 -0600

    added dot

--- src/db/connectionDb.ts
+++ src/db/connectionDb.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from "sequelize";
-
-export const sequelizeDB = new Sequelize("Prueba", "root", "root", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-});
+require("dotenv").config({ path: "../.env" });
+export const sequelizeDB = new Sequelize(
+  process.env.DB_NAME || "Prueba",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "root",
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: 3306,
+    dialect: "mysql",
+  }
+);
