d575f482f3aa07b4245288841f2c615a120abc76
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-06 20:21:01 -0600

    d

--- src/db/connectionDb.ts
+++ src/db/connectionDb.ts
@@ -3,11 +3,11 @@ import { Sequelize } from "sequelize";
 import { config } from "dotenv";
 config({ path: "../.env" });
 export const sequelizeDB = new Sequelize(
-  process.env.DB_NAME || "Prueba",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "root",
+  "jorgeluispenareal_prueba",
+  "254084_root	",
+  "gB0az0YYqK7wsSG",
   {
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: "mysql-jorgeluispenareal.alwaysdata.net",
     dialect: "mysql",
   }
 );
